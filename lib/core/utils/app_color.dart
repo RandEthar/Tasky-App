0e31054d3589e96f88244ee33df70a4d8d4b71ed
randethar <randethar@gmail.com>
2025-06-07 17:11:50 +0100

    create AppStyles & AppColors

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,14 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Colors.white;
+  static const Color secondary = Color(0xFF6076FF); 
+  static const Color background = Color(0xFF222338); 
+
+
+
+}
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static final TextStyle medium24 = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+  );
+  static final TextStyle medium14 = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static final TextStyle semiBold24 = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+   static final TextStyle semiBold14 = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+}
